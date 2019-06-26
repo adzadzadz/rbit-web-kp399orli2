471dba29efe14c5cca87808a7fab06ec29306bf5
Adrian Saycon <adzbite@gmail.com>
2019-06-27 03:56:42 +0800

    CORS fix

--- controllers/RestDefaultController.php
+++ controllers/RestDefaultController.php
@@ -15,9 +15,24 @@ class RestDefaultController extends \yii\rest\ActiveController
 {
     public $modelClass = 'app\models\User';
 
-    public function behaviors()
-    {
+    public function behaviors() {
         $behaviors = parent::behaviors();
+    
+        // remove authentication filter necessary because we need to 
+        // add CORS filter and it should be added after the CORS
+        unset($behaviors['authenticator']);
+    
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => '\yii\filters\Cors',
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
+        ];
+    
+        // re-add authentication filter of your choce
         $behaviors['authenticator'] = [
             'class' => CompositeAuth::className(),
             'except' => ['options', 'login'],
@@ -27,6 +42,7 @@ class RestDefaultController extends \yii\rest\ActiveController
                 QueryParamAuth::className(),
             ],
         ];
+    
         return $behaviors;
     }
 
